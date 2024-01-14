861d343811b26d41e27fdea54b2017671d356c87
Anastasia <StushaShilova@tut.by>
2024-01-14 16:57:45 +0300

    check_module file added

---
+++ hw_final/checking_module.py
@@ -0,0 +1,5 @@
+import files_work.format_functions
+from pathlib import Path
+
+res_1 = files_work.format_functions.json_to_pickle(Path('C:\\Users\\A\\OneDrive\\Документы\\Geek brains\\Python погружение\\seminars\\pickle_files'))
+res_2 = files_work.format_functions.dir_info ("C:\\Users\\A\\OneDrive\\Документы\\Geek brains\\Python погружение\\seminars")
